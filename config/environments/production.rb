2b826be547e07bcb227d7ef6368ab4a5f460049f
nborrego <nborrego@uccs.edu>
2022-04-16 20:15:47 +0000

    Updated environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://radiant-sea-45660.herokuapp.com/projects' }
 
 end
